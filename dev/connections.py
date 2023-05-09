28173488b7c71366d6769f8e50fcd72b2d884f4a
Célia Pereira <90316668+celiacapereira@users.noreply.github.com>
2023-05-09 16:41:07 +0100

    Update connections.py

--- dev/connections.py
+++ dev/connections.py
@@ -1,4 +1,12 @@
 import snowflake.connector
+import os
 from snowflake.connector import *
 import pandas as pd
 from snowflake.connector.pandas_tools import write_pandas
+
+conn = snowflake.connector.connect(
+    user=os.environ['user'],
+    password=os.environ['password'],
+    account=os.environ['account']
+    )
+
