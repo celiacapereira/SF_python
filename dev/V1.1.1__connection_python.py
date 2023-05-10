26fe34a6af3febd96e84effb3f7e2ae142231be1
celiacapereira <ccap98@gmail.com>
2023-05-10 16:02:57 +0100

    test entrou

--- dev/V1.1.1__connection_python.py
+++ dev/V1.1.1__connection_python.py
@@ -4,12 +4,14 @@ from snowflake.connector import *
 import pandas as pd
 from snowflake.connector.pandas_tools import write_pandas
 
+print('entrou')
+
 conn = snowflake.connector.connect(
     user=os.environ['SF_USERNAME'],
     password=os.environ['SF_PASSWORD'],
     account=os.environ['SF_ACCOUNT']
     )
-# print(conn.user)
+
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM DEV.RAW.TITANIC_TRAIN_RAW;")
 print("connection is donne")
