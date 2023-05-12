e18b824ccf4273f9eb2ff96602ada91a05b2d739
celiacapereira <ccap98@gmail.com>
2023-05-12 10:16:03 +0100

    new_pipe

--- dev/V1.1.1__connection_python.py
+++ dev/V1.1.1__connection_python.py
@@ -1,18 +1,50 @@
-import snowflake.connector
 import os
+import snowflake.connector
 from snowflake.connector import *
 import pandas as pd
 from snowflake.connector.pandas_tools import write_pandas
+import testes.connection as d
 
-print('entrou')
+account = os.environ['SF_ACCOUNT']
+password = os.environ['SF_PASSWORD']
+username= os.environ['SF_USERNAME']
 
-conn = snowflake.connector.connect(
-    user=os.environ['SF_USERNAME'],
-    password=os.environ['SF_PASSWORD'],
-    account=os.environ['SF_ACCOUNT']
-    )
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM DEV.RAW.TITANIC_TRAIN_RAW;")
-print("connection is donne")
-	
+print(f"SF ACCOUNT IS:  {account}")
+print(f"SF PASSWORD IS:  {password}")
+print(f"SF USERNAME IS:  {username}")
+
+print("Estamos na pasta dev")
+
+connection = snowflake.connector.connect (
+    user=username,
+    password=password,
+    account=account
+
+)
+cursor = connection.cursor()
+
+connection.cursor().execute("USE WAREHOUSE COMPUTE_WH;")
+
+connection.cursor().execute("CREATE OR REPLACE SCHEMA DEV.REPORT; ")
+
+connection.cursor().execute(""" CREATE TABLE DEV.REPORT.TITANIC_REPORT (
+    PASSENGERID INT,
+    SURVIVED INT, 
+    PCLASS INT, 
+    SEX VARCHAR(6), 
+    AGE INT, 
+    SIBSP INT, 
+    PARCH INT, 
+    AGERANGE VARCHAR(5)
+    )""")
+
+
+success, num_chunks, num_rows, output = write_pandas(
+            conn=connection,
+            df=d.df_drop,
+            table_name='TITANIC_REPORT',
+            database='DEV',
+            schema='REPORT'
+        )
+cursor.close()
